a3b22af231ec3eac85d0f507e3c31d331e8252a7
bob <shibo.zhou14@gmail.com>
2018-11-29 16:28:37 -0500

    torch.sigmoid() instead F.sigmoid()

--- activation.py
+++ activation.py
@@ -8,12 +8,12 @@ x = Variable(x)
 x_np = x.data.numpy()
 
 y_relu = F.relu(x).data.numpy()
-y_sigmoid = F.sigmoid(x).data.numpy()
-y_tanh = F.tanh(x).data.numpy()
+y_sigmoid = torch.sigmoid(x).data.numpy()
+y_tanh = torch.tanh(x).data.numpy()
 y_softplus = F.softplus(x).data.numpy()
 
 
-plt.figure(1, figsize(8,6))
+plt.figure(1, figsize=(8,6))
 plt.subplot(221)
 plt.plot(x_np, y_relu, c='red', label='relu')
 plt.ylim((-1, 5))
