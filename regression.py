9d746830538a96c233f92c0a1da43cc9a213ae8f
bob <shibo.zhou14@gmail.com>
2018-11-29 18:33:33 -0500

    Update

--- regression.py
+++ regression.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
 y = x.pow(2) + 0.2*torch.rand(x.size())
 
-x = Variable(x)
-y = Variable(y)
+#x = Variable(x)
+#y = Variable(y)
 
 # plt.scatter(x.data.numpy(), y.data.numpy())
 # plt.show()
