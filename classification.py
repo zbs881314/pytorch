f250c7891d3ca708f34f56d53df63369fc0f1402
bob <shibo.zhou14@gmail.com>
2018-11-29 19:21:17 -0500

    Update

--- classification.py
+++ classification.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 n_data = torch.ones(100, 2)
 x0 = torch.normal(2*n_data, 1)
@@ -21,11 +22,11 @@ class Net(torch.nn.Module):
     def __init__(self, n_feature, n_hidden, n_output):
         super(Net, self).__init__()
         self.hidden = torch.nn.Linear(n_feature, n_hidden)
-        self.predict = torch.nn.Linear(n_hidden, n_output)
+        self.out = torch.nn.Linear(n_hidden, n_output)
         
     def forward(self, x):
         x = F.relu(self.hidden(x))
-        x = self.predict(x)
+        x = self.out(x)
         return x
         
 net = Net(n_feature=2, n_hidden=10, n_output=2)
@@ -48,8 +49,7 @@ for t in range(200):
         prediction = torch.max(out, 1)[1]
         pred_y = prediction.data.numpy()
         target_y = y.data.numpy()
-        plt.scatter(x.data.numpy(), y.data.numpy())
-        plt.plot(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
         accuracy = float((pred_y == target_y).astype(int).sum())/float(target_y.size)
         plt.text(1.5, -4, 'Accuracy=%0.2f' % accuracy, fontdict={'size':20, 'color':'red'})
         plt.pause(0.1)
